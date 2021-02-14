cea09f5026eae804741dc50c5709400b9e7348aa
JDBennison <jamesbennison88@gmail.com>
2021-02-14 17:26:22 +0000

    Added faq page and content

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def faq(request):
+    """ A view to return the faq page """
+
+    return render(request, 'home/faq.html')
