d3d97b3a36564b7a61fa2467c97b4edd5a47abc1
JDBennison <jamesbennison88@gmail.com>
2021-03-05 16:06:05 +0000

    Added recent blog posts to home page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from blog.models import BlogPost
 
 # Create your views here.
+
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    blog_posts = BlogPost.objects.filter(status=1).order_by('-created_on')[:3]
+
+    context = {
+        "blog_posts": blog_posts
+    }
+
+    return render(request, 'home/index.html', context)
+
 
 def faq(request):
     """ A view to return the faq page """
