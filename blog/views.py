f1ca521116fdbbc7079c197f4081f445c8ec9b20
JDBennison <jamesbennison88@gmail.com>
2021-03-09 18:53:12 +0000

    Added blog search functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import BlogPost
 
 # Create your views here.
@@ -8,9 +10,23 @@ def all_blog_posts(request):
     """A view to show all blog posts"""
 
     blog_posts = BlogPost.objects.filter(status=1).order_by('-created_on')
+    blog_posts_sidebar = BlogPost.objects.filter(status=1).order_by('-created_on')
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('blog'))
+            
+            queries = Q(title__icontains=query) | Q(body__icontains=query)
+            blog_posts = blog_posts.filter(queries)
 
     context = {
-        "blog_posts": blog_posts
+        'blog_posts': blog_posts,
+        'blog_posts_sidebar': blog_posts_sidebar,
+        'search_term': query,
     }
 
     return render(request, 'blog/blog.html', context)
