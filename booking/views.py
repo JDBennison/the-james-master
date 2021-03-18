e01c9839451bae77780a1edd424a00538513710d
JDBennison <jamesbennison88@gmail.com>
2021-03-18 15:56:04 +0000

    Added checkout form data caching in payment intent

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-from django.shortcuts import get_object_or_404
 
 from .models import Booking, Order
 from .forms import OrderForm
@@ -9,6 +9,21 @@ from .forms import OrderForm
 import stripe
 
 # Create your views here.
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def booking(request):
@@ -87,6 +102,7 @@ def checkout(request):
             service = request.POST['service']
             date_id = request.POST['date']
             date = get_object_or_404(Booking, pk=date_id)
+            save_info = request.POST['save_info']
             location = request.POST['location']
             if service == 'IN':
                 cost = players * settings.INTRO_COST
@@ -116,7 +132,8 @@ def checkout(request):
                 'email': email,
                 'phone_number': phone_number,
                 'comment': comment,
-                'date_id': date_id
+                'date_id': date_id,
+                'save_info': save_info,
             }
 
             if not stripe_public_key:
