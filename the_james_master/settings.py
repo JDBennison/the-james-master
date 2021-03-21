956fcf2cb3f4729b49f682ad10d142d2e2ebe6e4
JDBennison <jamesbennison88@gmail.com>
2021-03-21 15:13:28 +0000

    Turned Debug on

--- the_james_master/settings.py
+++ the_james_master/settings.py
@@ -28,7 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-james-master.herokuapp.com', 'localhost']
 
